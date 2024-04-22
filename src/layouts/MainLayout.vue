4949b47def6f1840a2fa448f39320d2edf6ec9f0
Vitor Alves <vitor.dev.alves@gamil.com>
2024-04-22 18:31:14 -0300

    feat(first commit):

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,83 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          Vitor Alves
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Vitor Alves | Front-End
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksData = [
+  {
+    title: 'LinkedIN',
+    caption: 'Vitor Alves',
+    icon: 'school',
+    link: 'https://www.linkedin.com/in/vitor-alves-790006200/'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/vitoralves62',
+    icon: 'code',
+    link: 'https://github.com/vitoralves62'
+  }
+]
+
+export default {
+  name: 'MainLayout',
+  components: {
+    EssentialLink
+  },
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+  .q-header{
+    background-color: red;
+  }
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-page class="flex flex-center">
+    <!-- <h2>Portifólio</h2> -->
+    <h2>{{ projects[index] }}</h2>
+    <q-btn icon="autorenew" label="Roleta" stack glossy color="red" @click="randomPosition"/>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'ProjectsRollet',
+  data(){
+    return{
+      projects: [
+        "Sistema de Login",
+        "API jogadores",
+        "SPA de perfil",
+        "API livros",
+        "MemeBook"
+      ],
+      index: 0,
+    };
+  },
+  methods: {
+    randomPosition(){
+      let index = parseInt(Math.random() * 5 + 0);
+      console.log(index);
+      this.index = index;
+    }
+  }
+}
+</script>
